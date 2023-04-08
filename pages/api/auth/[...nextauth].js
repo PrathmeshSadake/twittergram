5d7f0d27b4f0f831a662631d9edece5067055acc
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-04-08 22:49:37 +0530

    Added client and server auth for protecting routes

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,12 +4,15 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 import prisma from "../../../prisma";
 
-export default NextAuth({
+export const authOptions = {
   adapter: PrismaAdapter(prisma),
+  //   secret: process.env.AUTH_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-});
+};
+
+export default NextAuth(authOptions);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  
+  return <div>
+
+  </div>;
 };
 
 export default Home;
