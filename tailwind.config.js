0e7bea0f8b90120ae02abec907ccced451bf4c41
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-04-26 21:41:18 +0530

    created prisma schema and migrated db

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      colors:{
-        socialBg:"#F5F7FB",
-        socialBlue:"#218DFA"
-      },
-
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
