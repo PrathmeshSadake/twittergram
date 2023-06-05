e38767ed9ba6bcf07ae96f8c5f22b7581fd1c5ed
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-06-05 20:44:09 +0530

    Created Sidebar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { Poppins } from "next/font/google";
+
+import Layout from "./components/Layout";
+
+import "@/styles/globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={poppins.className}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </main>
+  );
 }
---
+++ pages/components/Layout.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Sidebar from "./layout/Sidebar";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className='h-screen bg-black'>
+      <div className='container h-full mx-auto xl:px-30 max-w-6xl'>
+        <div className='grid grid-cols-4 h-full'>
+          <Sidebar />
+          <div className='col-span-3 lg:col-span-2 border-[1px] border-gray-800'>
+            {children}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
