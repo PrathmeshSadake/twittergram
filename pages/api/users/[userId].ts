136062d4060a29f165e3b82ace0daace16b63185
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-06-06 01:34:59 +0530

    Created api routes to fetch users and user by id

---
+++ pages/api/users/[userId].ts
@@ -0,0 +1,34 @@
+import { prisma } from "@/libs/prismadb";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "GET") {
+    return res.status(405).end();
+  }
+
+  try {
+    const { userId } = req.query;
+    if (!userId || typeof userId !== "string") {
+      throw new Error("Invalid ID");
+    }
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId as string,
+      },
+    });
+    const followersCount = await prisma.user.count({
+      where: {
+        followingIds: {
+          has: userId,
+        },
+      },
+    });
+    return res.status(200).json({ ...user, followersCount });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).end();
+  }
+}
