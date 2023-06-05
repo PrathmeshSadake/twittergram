97fc3049d271f5b17028232d5d28b91154766cea
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-06-05 20:01:29 +0530

    npx create-next-app@latest setup

--- src/pages/admin/[slug].js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const AdminPostEditPage = () => {
-  return <div>AdminPostEditPage</div>;
-};
-
-export default AdminPostEditPage;
--- src/pages/admin/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const AdminPostsPage = () => {
-  return <div>AdminPostsPage</div>;
-};
-
-export default AdminPostsPage;
