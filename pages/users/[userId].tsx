42f480e8491b6e8d58e7e42a20f7f65b522b3cfe
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-06-08 17:30:50 +0530

    showing user posts

--- pages/users/[userId].tsx
+++ pages/users/[userId].tsx
@@ -27,7 +27,7 @@ const UserView = () => {
       <Header showBackArrow label={fetchedUser?.name} />
       <UserHero userId={userId as string} />
       <UserBio userId={userId as string} />
-      {/* <PostFeed userId={userId as string} /> */}
+      <PostFeed userId={userId as string} />
     </>
   );
 };
